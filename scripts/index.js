eea1d917e8e8440e38e6f7b5c376e396de0c0c62
Marcus Sena <maarvins.19@gmail.com>
2021-11-30 18:45:30 -0300

    add initial js tests on the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
-//transformar HTML COLECTION EM ARRAY
-const hCollectionToArray = Array.from(document.getElementsByClassName("btn"))
-//HColection = os arrays da class respectiva
+let display = document.getElementById("input-display")
+let buttons = Array.from(document.getElementsByClassName("btn")) //utilizando o Array.from para converter uma coleção html em array ['']
+
+//addicionando o addEventListener em todos os botões (escutador) e retornando o valor do botão ao ser clicado
+
+buttons.map((btn) => {
+  btn.addEventListener("click", (e) => {
+    //TESTES DE FUNCIONAMENTO
+    // console.log("clicado!")
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.innerText)
+  })
+})
