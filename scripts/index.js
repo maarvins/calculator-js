ca02a692b055819d6fa1bc21bc4f0e0666c5e0da
Marcus Sena <maarvins.19@gmail.com>
2021-11-30 19:25:17 -0300

    feat: apply a logic in buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,35 @@
-let display = document.getElementById("input-display")
+let display = document.getElementById("display")
 let buttons = Array.from(document.getElementsByClassName("btn")) //utilizando o Array.from para converter uma coleção html em array ['']
 
 //addicionando o addEventListener em todos os botões (escutador) e retornando o valor do botão ao ser clicado
 
+//TESTES DE FUNCIONAMENTO
+// console.log("clicado!")
+// console.log(e)
+// console.log(e.target)
+// console.log(e.target.innerText)
+
 buttons.map((btn) => {
   btn.addEventListener("click", (e) => {
-    //TESTES DE FUNCIONAMENTO
-    // console.log("clicado!")
-    // console.log(e)
-    // console.log(e.target)
-    // console.log(e.target.innerText)
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = ""
+        break
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1)
+        }
+        break
+      case "=":
+        //eval ira considerar se sua equação matematica está correta para executá-la
+        try {
+          display.innerText = eval(display.innerText)
+        } catch {
+          display.innerHTML = "Error!"
+        }
+        break
+      default:
+        display.innerText += e.target.innerText
+    }
   })
 })
